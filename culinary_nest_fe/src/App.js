4d9ed8f14367d4888384bcf0aaf7be5066b2bf0d
Dimas Febriyanto <dimassfeb@gmail.com>
2023-09-24 12:58:51 +0700

    home page

--- culinary_nest_fe/src/App.js
+++ culinary_nest_fe/src/App.js
@@ -2,7 +2,16 @@ import "./App.css";
 import Home from "./pages/Home";
 
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <Home />;
+  return <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </>;
 }
--- culinary_nest_fe/src/pages/Home.jsx
+++ culinary_nest_fe/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import {Link} from "react-router-dom";
 
 export default function Home() {
   const fetchDataCategory = () => {
@@ -10,10 +11,18 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <div className="home">
-        <img className="bg-home" src="assets/home.png" alt="Culiner" />
-        <img src="assets/Title_CulinaryNest.png" alt="Title" />
+    <div className="container">
+      <div className="image-container">
+        <nav className="header" draggable="false">
+          <img src="assets/Logo.png" className="logo" alt="bg" draggable="false"/>
+          <Link to='/login' className="register_btn" draggable="false">Sign Up</Link>
+        </nav>
+        <img
+            src="assets/Title_CulinaryNest.png"
+            className="culinaryNestImage"
+            alt="bg"
+            draggable="false"
+        />
       </div>
     </div>
   );
