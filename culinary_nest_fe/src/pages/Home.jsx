6e0ebe0009537996e906493dad774d81e6c69b0c
Dimas Febriyanto <dimassfeb@gmail.com>
2023-09-26 22:02:53 +0700

    fixed height and size login register

--- culinary_nest_fe/src/pages/Home.jsx
+++ culinary_nest_fe/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const fetchDataCategory = () => {
@@ -15,33 +15,64 @@ export default function Home() {
       <div className="h-screen w-full bg-teal-500">
         <div className="bg-hero-home h-screen bg-cover justify-center items-center flex">
           <nav className="absolute top-0 flex w-full px-14 mt-5 items-center justify-between">
-            <img className="w-20 h-20" src="/assets/Logo.png"  draggable="false" alt="Logo"/>
-            <Link to="/login" className="px-8 py-2 bg-button-gray text-lg rounded-full">Log In</Link>
+            <img
+              className="w-20 h-20"
+              src="/assets/Logo.png"
+              draggable="false"
+              alt="Logo"
+            />
+            <Link
+              to="/login"
+              className="px-8 py-2 bg-button-gray text-lg rounded-full"
+            >
+              Log In
+            </Link>
           </nav>
-          <img className="absolute" src="/assets/Title_CulinaryNest.png"  draggable="false" alt="Logo"/>
+          <img
+            className="absolute px-5 sm:px-0 sm:w-1/2"
+            src="/assets/Title_CulinaryNest.png"
+            draggable="false"
+            alt="Logo"
+          />
           <div className="absolute bottom-0 translate-y-14 find-city shadow-2xl md:h-40 rounded-3xl md:w-1/2 bg-white">
             <div className="flex items-center justify-between mx-20 my-8">
               <p className="font-bold text-xl ">Enter your city</p>
               <div className="flex gap-5">
-                <div className="px-5 rounded-md bg-primary text-white">Destination</div>
-                <div className="px-5 rounded-md bg-primary text-white">Explore</div>
+                <div className="px-5 rounded-md bg-primary text-white">
+                  Destination
+                </div>
+                <div className="px-5 rounded-md bg-primary text-white">
+                  Explore
+                </div>
               </div>
             </div>
             <div className="mx-20 h-10 border-2 border-black rounded-full flex items-center px-5">
-              <img src="/assets/logos_google-maps.png" alt="maps" className="h-6 w-6"/>
+              <img
+                src="/assets/logos_google-maps.png"
+                alt="maps"
+                className="h-6 w-6"
+              />
               <div className="border-l-2 h-5 mx-2"></div>
-              <input className="w-full ml-2 outline-0" type="text" placeholder="Bali"/>
+              <input
+                className="w-full ml-2 outline-0"
+                type="text"
+                placeholder="Bali"
+              />
             </div>
           </div>
         </div>
       </div>
       <div className="flex  translate-y-20 px-20 justify-center items-center ">
         <div className="w-[886px]">
-          <img src="/assets/our_serve.png"  draggable="false" alt="Our Serve"/>
+          <img src="/assets/our_serve.png" draggable="false" alt="Our Serve" />
         </div>
         <div className="w-1/2 ">
           <div className="text-7xl font-bold">Our Serve</div>
-          <div className="text-lg text-justify ">Discover the perfect dining spot that matches your taste, budget, and preferences with trusted reviews, a variety of delicious dishes, and exclusive discounts all in one place.</div>
+          <div className="text-lg text-justify ">
+            Discover the perfect dining spot that matches your taste, budget,
+            and preferences with trusted reviews, a variety of delicious dishes,
+            and exclusive discounts all in one place.
+          </div>
         </div>
       </div>
     </>
--- culinary_nest_fe/src/pages/Register.jsx
+++ culinary_nest_fe/src/pages/Register.jsx
@@ -1,84 +1,109 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import TextField from "../components/TextField";
 import Button from "../components/Button";
-import {BASE_URL} from "../constants/BASE_URL";
+import { BASE_URL } from "../constants/BASE_URL";
 import axios from "axios";
-import {CardGlass} from "../components/CardGlass";
-import {Link} from "react-router-dom";
+import { CardGlass } from "../components/CardGlass";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (event)=> {
-        event.preventDefault();
-        try {
-            const res = await axios.post(BASE_URL + "/auth/login", {
-                email: email,
-                password: password,
-            })
-            if (res.status === 200) {
-                console.log("BERHASIL LOGIN")
-            } else {
-                throw Error("Gagal login")
-            }
-        } catch (e) {
-            console.log(e);
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post(BASE_URL + "/auth/login", {
+        email: email,
+        password: password,
+      });
+      if (res.status === 200) {
+        console.log("BERHASIL LOGIN");
+      } else {
+        throw Error("Gagal login");
+      }
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const handleChangeName = (event)=> {
-        setName(event.target.value);
-    }
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
 
-    const handleChangeEmail = (event)=> {
-        setEmail(event.target.value);
-    }
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleChangePassword = (event)=> {
-        setPassword(event.target.value);
-    }
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-    return (
-        <div className="h-screen w-full bg-primary">
-            <Link to={'/'}>
-                <div className="title flex gap-5 p-5 absolute">
-                    <img className="h-12 w-12" src="/assets/Logo.png" alt="Logo"/>
-                    <img className="h-12" src="/assets/Title_CulinaryNest.png" alt="Logo"/>
-                </div>
-            </Link>
-            <div className="w-full h-full flex items-center">
-                <div className="w-full md:w-1/2 px-5 md:px-10">
-                    <div className="flex flex-col justify-center gap-4 items-center">
-                        <div className="text-white text-7xl font-bold">Register</div>
-                        <div className="text-white text-sm text-center">
-                            Welcome to our platform! To get started, please fill out the following information to create your new account.
-                        </div>
-                    </div>
-                    <div className="w-full flex justify-center mt-20">
-                        <form className="flex flex-col w-full gap-5" onSubmit={handleSubmit}>
-                            <TextField label="Name" value={name} placeholder="Enter your name" onChange={handleChangeName}/>
-                            <TextField label="Email" value={email} placeholder="Enter your email" onChange={handleChangeEmail}/>
-                            <TextField label="Password" value={password} placeholder="Enter your password" onChange={handleChangePassword}/>
-                            <button type="submit">
-                                <Button title="Register"/>
-                            </button>
-                        </form>
-                    </div>
-                    <div className="flex flex-col justify-center items-center mt-10 text-white gap-4">
-                        <div>Don't have an account?</div>
-                        <Link to={'/login'} className="font-bold text-sm">Login Here!</Link>
-                    </div>
-                </div>
-                <div className="bg-hero-home hidden md:block sm:h-screen sm:w-1/2">
-                    <div className="absolute bottom-0 mt-auto">
-                        <CardGlass />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-screen w-full bg-primary">
+      <Link to={"/"}>
+        <div className="title flex gap-5 p-5 absolute">
+          <img className="h-12 w-12" src="/assets/Logo.png" alt="Logo" />
+          <img
+            className="h-12"
+            src="/assets/Title_CulinaryNest.png"
+            alt="Logo"
+          />
         </div>
-    );
+      </Link>
+      <div className="w-full h-full flex items-center">
+        <div className="w-full md:w-1/2 translate-y-8 px-5 md:px-10">
+          <div className="flex flex-col justify-center gap-4 items-center">
+            <div className="text-white text-5xl font-bold">Register</div>
+            <div className="text-white text-sm text-center">
+              Welcome to our platform! To get started, please fill out the
+              following information to create your new account.
+            </div>
+          </div>
+          <div className="w-full flex justify-center mt-5">
+            <form
+              className="flex flex-col w-full gap-5"
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                label="Name"
+                value={name}
+                placeholder="Enter your name"
+                onChange={handleChangeName}
+              />
+              <TextField
+                label="Email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={handleChangeEmail}
+              />
+              <TextField
+                label="Password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={handleChangePassword}
+              />
+              <button type="submit">
+                <Button title="Register" />
+              </button>
+            </form>
+          </div>
+          <div className="flex flex-col justify-center items-center mt-10 text-white gap-1">
+            <div>You have an account?</div>
+            <Link to={"/login"} className="font-bold text-sm">
+              Login Here!
+            </Link>
+          </div>
+        </div>
+        <div className="bg-hero-home hidden md:block sm:h-screen sm:w-1/2">
+          <div className="absolute bottom-0 mt-auto">
+            <CardGlass />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
